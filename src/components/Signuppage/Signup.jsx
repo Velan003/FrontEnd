20a305be320c549f50a6e76d4b4f0582eb1b9a9f
Velan003 <118647260+Velan003@users.noreply.github.com>
2023-10-28 11:23:48 +0530

    Update Signup.jsx

--- src/components/Signuppage/Signup.jsx
+++ src/components/Signuppage/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Signup.css';
+import './signup.css';
 
 const Signup = () => {
   const [formData, setFormData] = useState({
